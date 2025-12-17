f759aff178df81c7c90cb6326121eca6137bc854
kekeu_tk <kevyntaylor106@gmail.com>
2025-12-16 21:35:11 -0300

    akcar

--- carros/models.py
+++ carros/models.py
@@ -1,4 +1,11 @@
 from django.db import models
 
+class Carros(models.Model):
+    marca = models.CharField(max_length=100)
+    modelo = models.CharField(max_length=100)
+    ano = models.IntegerField()
+    preco = models.DecimalField(max_digits=10, decimal_places=2)
+    categoria = models.CharField(max_length=100)
 
-# Create your models here.
+    def __str__(self):
+        return f"{self.marca} {self.modelo} ({self.ano})"
