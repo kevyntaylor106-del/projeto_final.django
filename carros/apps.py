e8b725e6a6479323da27154e332d7aa94e588d79
kekeu_tk <kevyntaylor106@gmail.com>
2026-01-06 21:34:29 -0300

    final.Uc3.py

--- carros/apps.py
+++ carros/apps.py
@@ -1,14 +1,27 @@
+
 from django.apps import AppConfig
-from django.carros.singals import create_profile
-
-
+from django.utils.translation import gettext_lazy as _
 
 class CarrosConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'carros'
-class CarrosConfig(AppConfig):
-    name = 'carros'
-    class Meta:
-        verbose_name = "Carros"
+    verbose_name = _("Gestão de Carros")
 
     def ready(self):
-        import carros.signals  # Importa sinais para criar perfis automaticamente
+        # O único papel aqui é importar os sinais quando o Django iniciar
+        import carros.signals  # Certifique-se de que o arquivo signals.py existe
+
+
+        from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.conf import settings
+from .models import Perfil
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def gerenciar_perfil_usuario(sender, instance, created, **kwargs):
+    if created:
+        Perfil.objects.create(usuario=instance)
+    else:
+        # Garante que o perfil seja salvo se o User for atualizado
+        if hasattr(instance, 'perfil'):
+            instance.perfil.save()
--- carros/urls.py
+++ carros/urls.py
@@ -1,8 +1,21 @@
-from django.urls import path, include
-from django.contrib import admin
+# from django.urls import path, include
+# from django.contrib import admin
 
 
-urlpatterns = [
-    path('/', include('carros.urls')),
-    path('admin/', admin.site.urls),
-]
+# urlpatterns = [
+#     path('/', include('carros.urls')),
+#     path('admin/', admin.site.urls),
+# ]#
+
+from django.shortcuts import render, get_object_or_404
+from .models import Carro # Use o nome da classe em maiúsculo
+
+def lista_carros(request):
+    # Alterado o nome da variável para não conflitar com a classe
+    lista_de_carros = Carro.objects.all()
+    return render(request, 'carros/lista_carros.html', {'carros': lista_de_carros})
+
+def detalhe_carro(request, carro_id):
+    # Se o ID não existir, o Django exibe uma página 404 amigável
+    carro = get_object_or_404(Carro, id=carro_id)
+    return render(request, 'carros/detalhe_carro.html', {'carro': carro})
